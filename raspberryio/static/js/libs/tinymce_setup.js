35a7cb2b95986eba7d8261414af7ffcd5a5406cd
Caleb Smith <csmith@caktusgroup.com>
2013-02-05 08:58:45 -0500

    Refs #22 - Removed media and image upload buttons from TinyMCE editor

--- raspberryio/static/js/libs/tinymce_setup.js
+++ raspberryio/static/js/libs/tinymce_setup.js
@@ -47,7 +47,7 @@ if (typeof tinyMCE != 'undefined') {
         theme_advanced_toolbar_location: "top",
         theme_advanced_toolbar_align: "left",
         theme_advanced_statusbar_location: "",
-        theme_advanced_buttons1: "bold,italic,|,link,unlink,|,image,|,media,charmap,|,code,|,table,|,bullist,numlist,blockquote,|,undo,redo,|,formatselect,|,search,replace,|,fullscreen,",
+        theme_advanced_buttons1: "bold,italic,|,link,unlink,|charmap,|,code,|,table,|,bullist,numlist,blockquote,|,undo,redo,|,formatselect,|,search,replace,|,fullscreen,",
         theme_advanced_buttons2: "",
         theme_advanced_buttons3: "",
         theme_advanced_path: false,
