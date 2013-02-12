b7b85c41cb2b510284f1f98903a57914b96c599b
Vinod Kurup <vinod@kurup.com>
2013-02-12 13:53:27 -0500

    Add sekizai to TEMPLATE_CONTEXT_PROCESSORS

--- raspberryio/settings/base.py
+++ raspberryio/settings/base.py
@@ -105,6 +105,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
    "django.core.context_processors.tz",
     "mezzanine.conf.context_processors.settings",
+    'sekizai.context_processors.sekizai',
 )
 
 MIDDLEWARE_CLASSES = (
