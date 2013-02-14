a54df8d342097261bad9d5ca3042eb0655829202
Caleb Smith <csmith@caktusgroup.com>
2013-02-14 10:03:08 -0500

    Refs #45 - Made tests for search pass again, but dependent on settings

--- raspberryio/search/tests/test_models.py
+++ raspberryio/search/tests/test_models.py
@@ -7,18 +7,16 @@ from raspberryio.search.utils import load_search_model_indexes
 class SearchModelIndexeUtilsTestCase(ProjectBaseTestCase):
 
     def setUp(self):
-        self.index_settings = {
-            'auth.User': {
-                'first_name': 10,
-                'last_name': 5,
-            }
-        }
-        settings.SEARCH_MODEL_INDEXES = self.index_settings
+        """
+        FIXME: This test depends on the current settings, which has auth.user
+        in the SEARCH_MODEL_INDEXES setting.
+        """
+        self.index_settings = settings.SEARCH_MODEL_INDEXES
 
     def test_load_search_model_indexes_valid(self):
         search_proxy_model = load_search_model_indexes()[0]
         self.assertEqual(
             search_proxy_model.search_fields,
-            self.index_settings['auth.User']
+            self.index_settings['auth.user']
         )
         self.assertEqual(search_proxy_model.__name__, 'SearchableUser')
