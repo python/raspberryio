998df64f8784d48939b85819f1c323487ed3bd1b
juliaelman <julia.elman@gmail.com>
2013-02-17 15:37:48 -0500

    Adding a hover state for unfollowing users.

--- raspberryio/static/js/update_relationship.js
+++ raspberryio/static/js/update_relationship.js
@@ -1,27 +1,33 @@
-$(document).ready(function()
-    {
-        $(".follow-btn").delegate(".unfollow", "click", function(e) {
-             e.preventDefault();
-             var item = $(this);
-             $.post( $(this).attr("href"), function() {
-                     $(item).attr('class', 'follow')
-                            .text('Follow')
-                            .attr('href', $(item).attr('href').replace(/unfollow/i, "follow_all"));
-                     var followers = +$('#followers').data('followers');
-                     $('#followers').data('followers', followers - 1);
-                     $('#followers').text(followers - 1);
-                });
-          });
-        $(".follow-btn").delegate(".follow", "click", function(e) {
-             e.preventDefault();
-             var item = $(this);
-             $.post( $(this).attr("href"), function() {
-                     $(item).attr('class', 'unfollow')
-                            .text('Unfollow')
-                            .attr('href', $(item).attr('href').replace(/follow_all/i, "unfollow"));
-                     var followers = +$('#followers').data('followers');
-                     $('#followers').data('followers', followers + 1);
-                     $('#followers').text(followers + 1);
-             });
+$(function(){
+    $('.follow-btn').delegate('.unfollow', 'hover', function(event) {
+        if( event.type === 'mouseenter' )
+            $(this).text('Unfollow').addClass('action');
+        else
+            $(this).text('Following').removeClass('action');
+    });
+    $('.follow-btn').delegate('.unfollow', 'click', function(e) {
+        e.preventDefault();
+        var item = $(this);
+        $.post( $(this).attr("href"), function() {
+            $(item).attr('class', 'follow')
+            .text('Follow')
+            .attr('href', $(item).attr('href').replace(/unfollow/i, 'follow_all'));
+            var followers = +$('#followers').data('followers');
+            $('#followers').data('followers', followers - 1);
+            $('#followers').text(followers - 1);
+        });
+      });
+      
+    $('.follow-btn').delegate('.follow', 'click', function(e) {
+        e.preventDefault();
+        var item = $(this);
+        $.post( $(this).attr('href'), function() {
+            $(item).attr('class', 'unfollow')
+            .text('Following')
+            .attr('href', $(item).attr('href').replace(/follow_all/i, "unfollow"));
+            var followers = +$('#followers').data('followers');
+            $('#followers').data('followers', followers + 1);
+            $('#followers').text(followers + 1);
         });
     });
+});
