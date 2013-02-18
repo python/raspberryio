a30d79dad2677c80ebf1d1059528a8b5b46425f3
Caleb Smith <csmith@caktusgroup.com>
2013-02-18 17:51:15 -0500

    Refs #73 - Added QandA questions to search.

--- raspberryio/qanda/models.py
+++ raspberryio/qanda/models.py
@@ -15,7 +15,7 @@ class Question(Displayable, Ownable):
     question = RichTextField()
 
     def __unicode__(self):
-        return u'Question: {0}'.format(self.title)
+        return unicode(self.title)
 
     @models.permalink
     def get_absolute_url(self):
--- raspberryio/settings/base.py
+++ raspberryio/settings/base.py
@@ -294,6 +294,9 @@ SEARCH_MODEL_INDEXES = {
     'project.project': {
         'title': 10, 'tldr': 5,
     },
+    'qanda.question': {
+        'title': 10, 'question': 5,
+    },
 }
 
 ####################
