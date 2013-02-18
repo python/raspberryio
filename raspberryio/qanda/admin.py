508570bd5883aa7d5562f75816efbd7338d5f069
David Ray <davidray@gmail.com>
2013-02-18 09:00:41 -0500

    Adding filter to Answer Admin

--- raspberryio/qanda/admin.py
+++ raspberryio/qanda/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from raspberryio.qanda.models import Question, Answer
 
+class AnswerAdmin(admin.ModelAdmin):
+    model = Answer
+    list_filter = ('question',)
 
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Answer, AnswerAdmin)
