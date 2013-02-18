3a5d239ea2cf68fdfd22c4d6e918e2d985fed72e
Caleb Smith <csmith@caktusgroup.com>
2013-02-18 16:15:13 -0500

    Refs #81 - Fixed avatar tag by adding takes_context. Improved look of search results

--- raspberryio/userprofile/templatetags/profile_tags.py
+++ raspberryio/userprofile/templatetags/profile_tags.py
@@ -3,10 +3,11 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('includes/avatar.html')
-def avatar(profile, size=150):
-    return {
+@register.inclusion_tag('includes/avatar.html', takes_context=True)
+def avatar(context, profile, size=150):
+    context.update({
         'profile': profile,
         'profile_user': profile.user,
         'size': size,
-    }
+    })
+    return context
