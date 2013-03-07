6d098e4d2800b536f64ff5e297ba3ab6f716b0d0
Caleb Smith <csmith@caktusgroup.com>
2013-03-07 14:35:14 -0500

    Removing youtube url validation (at least for now) to check performance gains. Making this check isn't a big value add for the user and is costing us in performance. (Might add back if it doesn't prove helpful in further load tests)

--- raspberryio/project/forms.py
+++ raspberryio/project/forms.py
@@ -52,17 +52,6 @@ class ProjectForm(PlaceHolderMixin, forms.ModelForm):
             'title', 'featured_photo', 'featured_video', 'tldr', 'categories',
         )
 
-    def clean_featured_video(self):
-        data = self.cleaned_data.get('featured_video', '')
-        if data:
-            video_id = get_youtube_video_id(data)
-            try:
-                yt_service().GetYouTubeVideoEntry(video_id=video_id)
-            except:
-                msg = "The supplied URL is not a valid Youtube video"
-                raise forms.ValidationError(msg)
-        return data
-
 
 class ProjectStepForm(PlaceHolderMixin, forms.ModelForm):
 
