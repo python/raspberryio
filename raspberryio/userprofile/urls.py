a3f7edf15bec5fed1c7ea4da745683a69678c379
Caleb Smith <csmith@caktusgroup.com>
2013-03-08 11:14:31 -0500

    Overriding mezzanine login view to try setting csrf token in cookie for jmeter

--- raspberryio/userprofile/urls.py
+++ raspberryio/userprofile/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-from mezzanine.conf import settings
 
 
 urlpatterns = patterns('raspberryio.userprofile.views',
-
     # custom django-activity-stream based views
     url(r'^(?P<username>[\w.\-]+)/stream/$', 'profile_actions', name='profile-actions'),
     url(r'^(?P<username>[\w.\-]+)/(?P<relation>[followers|following]+)/$', 'profile_related_list', name='profile-related'),
 
+    # Override just the login view
+    url(r'^login/$', 'login', name='login'),
     url('^', include('mezzanine.accounts.urls')),
 )
