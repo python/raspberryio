b8fc7499211fc7d035ceba1a9f87d4afc7827c44
David Ray <davidray@gmail.com>
2013-03-11 08:21:30 -0400

    prepping prod settings for go-live settings, read from secrets.ini

--- raspberryio/settings/production.py
+++ raspberryio/settings/production.py
@@ -1,8 +1,22 @@
+import json
+import os
+
+from ConfigParser import RawConfigParser
+
 from raspberryio.settings.staging import *
 
-# There should be only minor differences from staging
+# import secrets
+try:
+    SECRETS_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir))
+    config = RawConfigParser()
+    config.read(os.path.join(SECRETS_ROOT, 'settings.ini'))
+    SUPERFEEDR_CREDS = json.loads(config.get('secrets', 'SUPERFEEDR_CREDS'))
+    DATABASES['default']['NAME'] = config.get('database', 'DATABASE_NAME')
+    DATABASES['default']['HOST'] = config.get('database', 'DATABASE_HOST')
+    DATABASES['default']['USER'] = config.get('database', 'DATABASE_USER')
+    DATABASES['default']['PASSWORD'] = config.get('database', 'DATABASE_PASSWORD')
 
-DATABASES['default']['NAME'] = 'raspberryio_production'
+except:
+    pass
 
 EMAIL_SUBJECT_PREFIX = '[Raspberryio Prod] '
-
